2186e40223e7a11b23821a8df2ae5870de62e38a
Oussema <oussema.ber11@gmail.com>
2021-12-06 16:25:33 +0100

    add tables

---
+++ data/seeds/001_first-seed.js
@@ -0,0 +1,16 @@
+const profiles = [ {
+  name: 'Imen' },
+  {
+  name: 'Aymen'
+  }, {
+  name: 'Feres' },
+  {
+  name: 'Nadia'
+  }, {
+  name: 'Mohamed' }
+  ];
+
+  exports.seed = function (knex) { 
+  return knex('profiles').del() .then(() => {
+  return knex('profiles').insert(profiles)
+  }) };
